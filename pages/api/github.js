5cb5676f24226624c4c2fe1aa9f10f14bdfd6c7d
spences10 <spences10apps@gmail.com>
2020-11-04 21:13:16 +0000

    add octokit

---
+++ pages/api/github.js
@@ -0,0 +1,29 @@
+const { Octokit } = require("@octokit/rest");
+
+export default async (req, res) => {
+  const user = req.query.username;
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_AUTH_TOKEN,
+  });
+
+  const followers = await octokit.request(
+    `/users/${user}/followers?per_page=1000`
+  );
+  const followerCount = followers.data.length;
+
+  const stars = await octokit.request(`/users/${user}/repos`);
+  const starsCount = stars.data
+    .filter((repo) => !repo.fork)
+    .reduce((acc, item) => {
+      return acc + item.stargazers_count;
+    }, 0);
+
+  const reposStarred = await octokit.request(`/users/${user}/starred`);
+  const starredCount = reposStarred.data.length;
+
+  return res.status(200).json({
+    stars: starsCount,
+    followers: followerCount,
+    starred: starredCount,
+  });
+};
